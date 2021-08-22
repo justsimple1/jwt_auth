6725d79a2d37c3f8a886bc4715098ff02808f13f
justsimple1 <dla-raboti5@yandex.ru>
2021-08-22 16:09:03 +0300

    add services, add controllers

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
 const express = require('express');
-const cors = require ('cors');
-const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const cookieParser = require('cookie-parser')
 const mongoose = require('mongoose');
+const router = require('./router/index')
+
+
 const PORT = process.env.PORT || 5000;
-const app = express()
-const router = require('router/index')
+const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
